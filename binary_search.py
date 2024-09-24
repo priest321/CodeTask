588a4d6b6ddddc9be14cdb9b1c01ddedeac532e4
Robert Yang <priest321@hotmail.com>
2024-09-24 22:36:33 +1000

    Update binary search

--- binary_search.py
+++ binary_search.py
@@ -1,26 +1,30 @@
-def binary_search(data:list, target):
-    def search(partial_data, target):
-        if not partial_data:
-            return False
-            
-        size = len(partial_data)
-        mid = size//2
-        print(partial_data, target, mid)
-        if partial_data[mid] == target:
-            return True
-        elif partial_data[mid] < target:
-            return search(partial_data[mid:], target)
+def search(data, target):
+    def binary_search(data, low, high, t):
+        if high>=low:
+            mid = (low+high)//2
+            if data[mid] == t:
+                return mid
+            elif data[mid] < target:
+                return binary_search(data, mid+1, high, t)
+            else:
+                return binary_search(data, low, mid-1, t)
         else:
-            return search(partial_data[:mid], target)
-    
-    return search(data, target)
+            return -1
+    return binary_search(data, 0, len(data)-1, target)
 	
 
 
-print(binary_search([1], 1))
-print(binary_search([1,2], 1))
-print(binary_search([1,2,5,9], 5))
+print(search([1], 1))
+print(search([1,2], 1))
+print(search([1,2,5,9], 5))
 
-print(binary_search([1,2,3,4,5,6,7,8,9], 5))
+print(search([1,2,3,4,5,6,7,8,9], 5))
 
-print(binary_search([1,2,3,4,5,7,8,9,10,11], 6))
+print(search([1,2,3,4,5,7,8,9,10,11], 6))
+
+
+
+
+
+
+
